f6825f63b16822ea3fa140c3e8d4e34c03cb8a46
Ruben David <ruben@jobs2live.com>
2024-06-05 17:19:45 -0400

    Fernando Herrera Sec-12 05 Modulos y componentes y iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,ErrorPageComponent],
   imports: [BrowserModule],
   providers: [
     provideAnimationsAsync()
