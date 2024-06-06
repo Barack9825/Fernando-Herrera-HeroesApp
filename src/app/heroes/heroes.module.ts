773c71ade366a39027d589c5954e257c721e9ed0
Ruben David <ruben@jobs2live.com>
2024-06-05 21:25:49 -0400

    Fernando Herrera Sec-12 10 Mostrar Rutas Hijas -Segundo Router Outlet

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HeroesRoutingModule } from './heroes-routing.module';
+
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+
 
 @NgModule({
   declarations: [
@@ -15,6 +18,6 @@ import { ListadoComponent } from './pages/listado/listado.component';
     HomeComponent,
     ListadoComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule,HeroesRoutingModule],
 })
 export class HeroesModule {}
